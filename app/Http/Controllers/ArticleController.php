0ddbe4a387683b9ac301460a93db547b2debdbc1
Toni Covas <antoniocovas@paucasesnovescifp.cat>
2026-04-26 03:09:00 +0200

    Endpoints d'articles verificats amb Postman

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    // ENDPOINT PÚBLIC 1: Llistar tots els articles
+    public function index()
+    {
+        return response()->json(Article::all(), 200);
+    }
+
+    // ENDPOINT PRIVAT 1: Crear un nou article (Protegit amb Token)
+    public function store(Request $request)
+    {
+        $article = Article::create($request->all());
+        return response()->json($article, 201);
+    }
+
+    // ENDPOINT PÚBLIC 2: Veure un article concret
+    public function show($id)
+    {
+        $article = Article::find($id);
+        if (!$article) return response()->json(['m' => 'No trobat'], 404);
+        return response()->json($article, 200);
+    }
+
+    // ENDPOINT PRIVAT 2: Esborrar un article (Protegit amb Token)
+    public function destroy($id)
+    {
+        Article::destroy($id);
+        return response()->json(['m' => 'Article eliminat'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,17 @@ Route::get('/user', function (Request $request) {
 // Endpoints 1 i 2: Registre i Login (Públics)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+
+use App\Http\Controllers\ArticleController;
+
+// Rutes Públiques
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
+
+// Rutes Privades (Requereixen Bearer Token)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/articles', [ArticleController::class, 'store']);
+    Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
+});
+
